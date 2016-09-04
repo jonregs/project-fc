448819b1d79e3be1fdc288befe5cc6741b966f6a
Jonathan Jude Regalado <jonegalado@Jonathans-MacBook-Pro.local>
2016-09-04 16:37:04 -0700

    Initial commit

---
+++ app/src/main/java/com/rega/fc/FoodOffer.java
@@ -0,0 +1,43 @@
+package com.rega.fc;
+
+/**
+ * Created by jonegalado on 9/4/16.
+ */
+public class FoodOffer {
+    private String name;
+    private int numOfSongs;
+    private int thumbnail;
+
+    public FoodOffer() {
+    }
+
+    public FoodOffer(String name, int numOfSongs, int thumbnail) {
+        this.name = name;
+        this.numOfSongs = numOfSongs;
+        this.thumbnail = thumbnail;
+    }
+
+    /*public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumOfSongs() {
+        return numOfSongs;
+    }
+
+    public void setNumOfSongs(int numOfSongs) {
+        this.numOfSongs = numOfSongs;
+    }*/
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
---
+++ app/src/main/java/com/rega/fc/FoodOfferAdapter.java
@@ -0,0 +1,105 @@
+package com.rega.fc;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import java.util.List;
+
+/**
+ * Created by jonegalado on 9/4/16.
+ */
+public class FoodOfferAdapter extends RecyclerView.Adapter<FoodOfferAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<FoodOffer> foodOfferList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, count;
+        public ImageView thumbnail;
+
+        public MyViewHolder(View view) {
+            super(view);
+            //title = (TextView) view.findViewById(R.id.title);
+            //count = (TextView) view.findViewById(R.id.count);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+            //overflow = (ImageView) view.findViewById(R.id.overflow);
+        }
+    }
+
+
+    public FoodOfferAdapter(Context mContext, List<FoodOffer> foodOfferList) {
+        this.mContext = mContext;
+        this.foodOfferList = foodOfferList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.album_card, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        FoodOffer foodOffer = foodOfferList.get(position);
+        //holder.title.setText(foodOffer.getName());
+        //holder.count.setText(foodOffer.getNumOfSongs() + " songs");
+
+        // loading album cover using Glide library
+        Glide.with(mContext).load(foodOffer.getThumbnail()).into(holder.thumbnail);
+
+        /* holder.overflow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(holder.overflow);
+            }
+        }); */
+    }
+
+    /**
+     * Showing popup menu when tapping on 3 dots
+     */
+    /*private void showPopupMenu(View view) {
+        // inflate menu
+        PopupMenu popup = new PopupMenu(mContext, view);
+
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_album, popup.getMenu());
+        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        popup.show();
+    }*/
+
+    /**
+     * Click listener for popup menu items
+     */
+    /*class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+
+        public MyMenuItemClickListener() {
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_add_favourite:
+                    Toast.makeText(mContext, "Add to favourite", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.action_play_next:
+                    Toast.makeText(mContext, "Play next", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+            }
+            return false;
+        }*/
+
+    @Override
+    public int getItemCount() {
+        return foodOfferList.size();
+    }
+}
